518942ac69512ea4711c42dc259ffa5295be339e
guikastner <guilherme@kastner.com.br>
2025-10-30 16:06:40 -0300

    Lesson 3 - Término da parte de listas - Apenas app.vue

--- vueschool/src/App.vue
+++ vueschool/src/App.vue
@@ -2,9 +2,24 @@
 import { ref } from 'vue'
 
 const header = ref('Shopping list app')
+const items = ref([
+  { id: 1, text: 'Apples' },
+  { id: 2, text: 'Oranges' },
+  { id: 3, text: 'Bananas' },
+  { id: 4, text: 'Grapes' },
+])
+// const items = ref(['Apples',
+//         'Oranges',
+//         'Bananas',
+//         'Grapes'])
 </script>
 
 <template>
   <h1>{{ header.toLocaleUpperCase() }}</h1>
-  <!-- <input v-model="header" /> -->
+  <ul>
+    <!-- visualização de array de objetos -->
+    <li v-for="({id, text}, index) in items" :key="id">
+      {{ index + 1 }} - {{text }}
+    </li>
+  </ul>
 </template>
